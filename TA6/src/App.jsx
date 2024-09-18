60d47cfa5ddcfd5fe70486d7e558531ca5191776
longuigon <longuigon+git@gmail.com>
2024-09-18 10:23:07 -0300

    cambiado para usar components

--- TA6/src/App.jsx
+++ TA6/src/App.jsx
@@ -1,30 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { Card } from './components/card.jsx';
 
 function App() {
-  const [tareas, setTareas] = useState([]);
-  const [nuevaTarea, setNuevaTarea] = useState("");
-
-  const addTarea = (e) => {
-    e.preventDefault();
-    if (nuevaTarea.trim()) {
-      setTareas([...tareas, nuevaTarea]);
-      setNuevaTarea('');
-    }
-  }
-
   return (
-    <>
-      <form onSubmit={addTarea}>
-        <input type="text" className="taskInput" id="newTask" placeholder="Ingresa una tarea..." value={nuevaTarea} onChange={(e) => setNuevaTarea(e.target.value)}></input>
-        <button type="submit">Agregar</button>
-      </form>
-      <ul>
-        {tareas.map((tarea, index) => (
-          <li key={index} className="taskCard">{tarea}</li>
-        ))}
-      </ul>
-    </>
+    <Card />
   )
 }
 
